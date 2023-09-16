8a6a7b11d1a335edb7fad2db6a8ebd072f636f6b
philipp-schuetz <mail@philippschuetz.com>
2023-09-16 12:13:46 +0200

    Add ParticipantsTable

---
+++ frontend/src/routes/groups/[groupName]/ParticipantsTable.svelte
@@ -0,0 +1,34 @@
+<script>
+export let participantsData;
+console.log(participantsData);
+</script>
+<table class="table table-striped">
+	<thead>
+		<tr>
+			<th scope="col">Vorname</th>
+			<th scope="col">Nachname</th>
+			<th scope="col">Anwesend</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each participantsData as participant (participant.firstname)} <!-- api should provide a unique id for each participant, use this as and identifiere here instead of firstname-->
+			<tr>
+				<td>{participant.firstname}</td>
+				<td>{participant.lastname}</td>
+				{#if participant.present === true}
+					<td>
+						<div class="form-check">
+							<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+						</div>
+					</td>
+				{:else}
+					<td>
+						<div class="form-check">
+							<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+						</div>
+					</td>
+				{/if}
+			</tr>
+		{/each}
+	</tbody>
+  </table>
