74d889d9f801f67617ebc44a8135b781e277007c
philipp-schuetz <mail@philippschuetz.com>
2023-09-23 21:26:30 +0200

    Add Navbar component

---
+++ frontend/src/routes/Navbar.svelte
@@ -0,0 +1,90 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+		<a
+			class="navbar-brand"
+			href="/">MNMS</a
+		>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div
+			class="collapse navbar-collapse"
+			id="navbarSupportedContent"
+		>
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a
+						class="nav-link"
+						aria-current="page"
+						href="/evaluation">Auswertung</a
+					>
+				</li>
+				<li class="nav-item dropdown">
+					<a
+						class="nav-link dropdown-toggle"
+						href="/points"
+						role="button"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
+					>
+						Punkte
+					</a>
+					<ul class="dropdown-menu">
+						<li>
+							<a
+								class="dropdown-item"
+								href="/points/7midi-1">7midi-1</a
+							>
+						</li>
+						<li>
+							<a
+								class="dropdown-item"
+								href="/points/7midi-2">7midi-2</a
+							>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item dropdown">
+					<a
+						class="nav-link dropdown-toggle"
+						href="/points"
+						role="button"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
+					>
+						Anwesenheit
+					</a>
+					<ul class="dropdown-menu">
+						<li>
+							<a
+								class="dropdown-item"
+								href="/presence/7midi-1">7midi-1</a
+							>
+						</li>
+						<li>
+							<a
+								class="dropdown-item"
+								href="/presence/7midi-2">7midi-2</a
+							>
+						</li>
+						<li><hr class="dropdown-divider" /></li>
+						<li>
+							<a
+								class="dropdown-item"
+								href="/presence/check">Überprüfen</a
+							>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
