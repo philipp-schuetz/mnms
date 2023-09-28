8b512683ca47867e90d681756709047a0f59fa4f
philipp-schuetz <mail@philippschuetz.com>
2023-09-28 19:40:58 +0200

    Fix formatting of points page

--- frontend/src/routes/points/[groupName]/+page.svelte
+++ frontend/src/routes/points/[groupName]/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-	import { fetchData } from '../../../api.js'
+	import { fetchData } from '../../../api.js';
 	import StationsTable from './StationsTable.svelte';
 
 	const groupName = $page.params.groupName;
-	const className = groupName.replace(/-\d$/, '')
+	const className = groupName.replace(/-\d$/, '');
 
 	let classData = {};
 
@@ -13,17 +13,19 @@
 		classData = await fetchData(`/classes/info?class_name=${className}`);
 	});
 </script>
+
 <div class="container">
-	<div class="row">
-	  <div class="col">
-		<StationsTable {groupName} />
-	  </div>
-	  <div class="col">
-		<span class="bold">Klassenraum:</span> {classData.room} <br>
-		<span class="bold">KlassenlehrerIn:</span> {classData.teacher}
-	  </div>
+	<div class="col">
+		<div class="row">
+			<span class="bold">Klassenraum: {classData.room}</span>
+			<span class="bold">KlassenlehrerIn: {classData.teacher}</span>
+		</div>
+		<div class="row">
+			<StationsTable {groupName} />
+		</div>
 	</div>
-  </div>
+</div>
+
 <style>
 	.bold {
 		font-weight: bold;
