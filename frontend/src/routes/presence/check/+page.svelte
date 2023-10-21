1cc9554fc3fedefbbf984a7c622033782f6ba46d
philipp-schuetz <mail@philippschuetz.com>
2023-10-21 14:06:36 +0200

    Sort participants by group in presence check

--- frontend/src/routes/presence/check/+page.svelte
+++ frontend/src/routes/presence/check/+page.svelte
@@ -7,8 +7,22 @@
 
 	onMount(async () => {
 		participantsData = await fetchData(`/participants/get-all`);
+		participantsData = sortByGroup(participantsData);
 	});
+
+	function sortByGroup(persons) {
+		return persons.sort((a, b) => {
+			if (a.group < b.group) {
+				return -1;
+			}
+			if (a.group > b.group) {
+				return 1;
+			}
+			return 0;
+		});
+	}
 </script>
+
 <div class="container">
 	<div class="col">
 		<div class="row">
@@ -16,66 +30,65 @@
 		</div>
 		<div class="row">
 			<button
-	type="button"
-	class="btn btn-primary"
-	on:click={() => (not_present_switch = !not_present_switch)}
->
-	{#if not_present_switch}
-		Abwesend
-	{:else}
-		Alle
-	{/if}
-</button>
-<table class="table table-striped">
-	<thead>
-		<tr>
-			<th scope="col">Gruppe</th>
-			<th scope="col">Vorname</th>
-			<th scope="col">Nachname</th>
-			<th scope="col">Anwesend</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each participantsData as participant}
-			{#if not_present_switch}
-				{#if !participant.present}
-					<tr>
-						<td>{participant.group}</td>
-						<td>{participant.firstname}</td>
-						<td>{participant.lastname}</td>
-						<td>
-							<div class="form-check">
-								<input
-									class="form-check-input"
-									type="checkbox"
-									bind:checked={participant.present}
-									disabled
-								/>
-							</div>
-						</td>
-					</tr>
+				type="button"
+				class="btn btn-primary"
+				on:click={() => (not_present_switch = !not_present_switch)}
+			>
+				{#if not_present_switch}
+					Abwesend
+				{:else}
+					Alle
 				{/if}
-			{:else}
-				<tr>
-					<td>{participant.group}</td>
-					<td>{participant.firstname}</td>
-					<td>{participant.lastname}</td>
-					<td>
-						<div class="form-check">
-							<input
-								class="form-check-input"
-								type="checkbox"
-								bind:checked={participant.present}
-								disabled
-							/>
-						</div>
-					</td>
-				</tr>
-			{/if}
-		{/each}
-	</tbody>
-</table>
-
+			</button>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th scope="col">Gruppe</th>
+						<th scope="col">Vorname</th>
+						<th scope="col">Nachname</th>
+						<th scope="col">Anwesend</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each participantsData as participant}
+						{#if not_present_switch}
+							{#if !participant.present}
+								<tr>
+									<td>{participant.group}</td>
+									<td>{participant.firstname}</td>
+									<td>{participant.lastname}</td>
+									<td>
+										<div class="form-check">
+											<input
+												class="form-check-input"
+												type="checkbox"
+												bind:checked={participant.present}
+												disabled
+											/>
+										</div>
+									</td>
+								</tr>
+							{/if}
+						{:else}
+							<tr>
+								<td>{participant.group}</td>
+								<td>{participant.firstname}</td>
+								<td>{participant.lastname}</td>
+								<td>
+									<div class="form-check">
+										<input
+											class="form-check-input"
+											type="checkbox"
+											bind:checked={participant.present}
+											disabled
+										/>
+									</div>
+								</td>
+							</tr>
+						{/if}
+					{/each}
+				</tbody>
+			</table>
 		</div>
 	</div>
 </div>
